b739a5e14110e5d3bc0169f296247a585a5c74a0
Ulric Todman <rick.sinclair.nyc@gmail.com>
2022-07-17 13:23:51 -0400

    removed unused import

--- UlricTodman.com/src/App.tsx
+++ UlricTodman.com/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {useEffect} from "react";
 import BusinessCard from "./components/BusinessCard/BusinessCard";
+import QRCode from "./assets/QRCODE.png"
 
 function App() {
 
@@ -15,6 +16,11 @@ function App() {
     return (
         <div className="App" id="application">
             <BusinessCard  defaultValues/>
+            <div className="small-text">
+                <img src={QRCode} width = "317" className="qr" height="auto" alt="QR Code"/>
+                <small>made with react.js and vite</small>
+                <small>&copy; 2022 Ulric Todman</small>
+            </div>
 
         </div>
 
